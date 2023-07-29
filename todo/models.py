27022f086a7c77209d42f12876602cd552883141
Ajay <majay1638@gmail.com>
2023-07-29 18:31:31 +0530

    Merge master into main

--- todo/models.py
+++
@@ -1,9 +0,0 @@
-from operator import mod
-from django.db import models
-
-# Create your models here.
-
-
-class Todo(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
--- todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("", home, name="home"),
-    path('todos/', TodoListCreateView.as_view(), name='todo-list-create'),
-    path('todos/<int:pk>/', TodoRetrieveUpdateDeleteView.as_view(),
-         name='todo-retrieve-update-delete'),
-]
